0dbf8d28bbdc79aa46dce11eaf106a70536df756
Allen Shen <ashen18@ucsc.edu>
2024-11-02 16:42:51 -0700

    delete js changes

---
+++ script.js
@@ -0,0 +1,14 @@
+let billamount = 0;
+let numpeople = 0;
+let custompercent = 0;
+let tipamount = 0;
+let totalamount = 0;
+
+
+function resettips() {
+    billamount = 0;
+    numpeople = 0;
+    custompercent = 0;
+    tipamount = 0;
+    totalamount = 0;
+}
